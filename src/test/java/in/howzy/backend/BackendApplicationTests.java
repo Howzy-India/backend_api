387d2c1f8acd9f95eed824ace4ec7197c7c6ce06
gouthamkumarpagadala <gouthamkumarp@outlook.com>
2026-03-08 12:35:03 +0530

    updated codebase with working api

---
+++ src/test/java/in/howzy/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.howzy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
